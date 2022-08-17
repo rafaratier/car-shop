cef664936c069a43315115bc081b544713249677
Rafael <rafaelratier@hotmail.com>
2022-08-16 23:01:31 -0300

    test(carmodeltest): adds test to method findOne

--- src/tests/unit/models/carModelTest.test.ts
+++ src/tests/unit/models/carModelTest.test.ts
@@ -10,20 +10,29 @@ describe('Test CarModel', () => {
   const carModel = new CarModel();
 
   before(async () => {
-    sinon
-      .stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
   });
 
   after(() => {
     sinon.restore();
   })
 
-  describe("tests if it's possible to register a new car", () => {
+  describe("CREATE - tests if it's possible to register a new car", () => {
 
     it('by passing the rigth args', async () => {
       const newCar = await carModel.create(carMock);
 
       expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+  });
+
+  describe("FIND_ONE - tests if it's possible to find registry of a car", () => {
+
+    it('by passing the rigth _id as args', async () => {
+      const carFound = await carModel.readOne('62261a65d66c6be0a63c051f');
+
+      expect(carFound).to.be.deep.equal(carMockWithId);
     });
   });
 
