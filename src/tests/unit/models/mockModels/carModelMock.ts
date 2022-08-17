626cc2972b005cddd0316537a23cc7c703f11a99
Rafael <rafaelratier@hotmail.com>
2022-08-16 23:27:29 -0300

    test(carmodel): adds test to method find of carModel

--- src/tests/unit/models/mockModels/carModelMock.ts
+++ src/tests/unit/models/mockModels/carModelMock.ts
@@ -8,7 +8,7 @@ const carMock: ICar = {
   doorsQty: 2,
   seatsQty: 2,
   status: true,
-}
+};
 
 const carMockWithId: ICar & { _id: string } = {
   _id: '62261a65d66c6be0a63c051f',
@@ -19,9 +19,33 @@ const carMockWithId: ICar & { _id: string } = {
   doorsQty: 2,
   seatsQty: 2,
   status: true,
-}
+};
+
+const carMockList: ICar[] & { _id: string }[] = [
+  {
+    _id: '62261a65d66c6be0a63c051f',
+    model: 'Ferrari 458 Italia',
+    year: 2015,
+    color: 'red',
+    buyValue: 270000000,
+    doorsQty: 2,
+    seatsQty: 2,
+    status: true,
+  },
+  {
+    _id: '1097a65d77c2be0a63c091p',
+    model: 'Ferrari F8 Tributo',
+    year: 2015,
+    color: 'red',
+    buyValue: 419257400,
+    doorsQty: 2,
+    seatsQty: 2,
+    status: true,
+  }
+];
 
 export {
   carMock,
-  carMockWithId
+  carMockWithId,
+  carMockList
 };
